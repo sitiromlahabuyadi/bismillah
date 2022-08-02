f24b6690ff4c97232f361f1cfad5e5027e836e9e
Leni Fadhilah <107199663+LeniFadhilah@users.noreply.github.com>
2022-08-02 15:55:26 +0700

    Update Model_dokter.php

--- application/models/Model_dokter.php
+++ application/models/Model_dokter.php
@@ -4,10 +4,10 @@ Class Model_dokter extends CI_Model {
 
     function add($foto) {
         $data = array(
-            'nama_dokter' => $this->input->post('nama_dokter'),
-            'alamat' => $this->input->post('alamat'),
-            'jenis_dokter' => $this->input->post('jenis'),
-            'no_hp' => $this->input->post('no_hp'),
+            'Linny_fadhilah' => $this->input->post('Linny_fadhilah'),
+            'Situbondo' => $this->input->post('Situbondo'),
+            'Poli_kulit' => $this->input->post('Poli_kulit'),
+            'Sesi_1' => $this->input->post('Sesi_1'),
             'foto' => $foto
         );
         $this->db->insert('tbl_dokter', $data);
@@ -16,10 +16,10 @@ Class Model_dokter extends CI_Model {
     function edit($foto) {
         if (empty($foto)) {
             $data = array(
-                'nama_dokter' => $this->input->post('nama_dokter'),
-                'alamat' => $this->input->post('alamat'),
-                'jenis_dokter' => $this->input->post('jenis'),
-                'no_hp' => $this->input->post('no_hp'),
+                'Sofiyatu_laili' => $this->input->post('Sofiyatul_laili'),
+                'Situbondo' => $this->input->post('Situbondo'),
+                'Poli_gigi' => $this->input->post('Poli_gigi'),
+                'Sesi_2' => $this->input->post('Sesi_2'),
             );
             $id_dokter = $this->input->post('id_dokter');
             $this->db->where('id_dokter', $id_dokter);
